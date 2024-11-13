2aecf0e8ff832256296a582aa31cb808a0c55184
emythri <eelapantimythri@gmail.com>
2024-11-13 17:23:25 +0530

    render

--- Survey/calc/views.py
+++ Survey/calc/views.py
@@ -14,6 +14,21 @@ def survey_list(request):
     surveys = Survey.objects.all()
     return render(request, 'survey_list.html', {'surveys': surveys})
 
+# def survey_detail(request, survey_id):
+#     survey = get_object_or_404(Survey, id=survey_id)
+#     questions = survey.questions.all()
+
+#     if request.method == 'POST':
+#         form = SurveyResponseForm(request.POST, survey_questions=questions)
+#         if form.is_valid():
+#             score = calculate_score(form.cleaned_data)
+#             return render(request, 'results.html', {'score': score})
+#     else:
+#         form = SurveyResponseForm(survey_questions=questions)
+
+#     return render(request, 'survey_detail.html', {'survey': survey, 'form': form})
+
+
 def survey_detail(request, survey_id):
     survey = get_object_or_404(Survey, id=survey_id)
     questions = survey.questions.all()
@@ -21,7 +36,8 @@ def survey_detail(request, survey_id):
     if request.method == 'POST':
         form = SurveyResponseForm(request.POST, survey_questions=questions)
         if form.is_valid():
-            score = calculate_score(form.cleaned_data)
+            responses = form.cleaned_data
+            score = calculate_score(responses)
             return render(request, 'results.html', {'score': score})
     else:
         form = SurveyResponseForm(survey_questions=questions)
@@ -30,20 +46,43 @@ def survey_detail(request, survey_id):
 
 @login_required(login_url='login') 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True) 
+# def create_survey(request):
+#     if request.method == 'POST':
+#         form = SurveyForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('survey_list')
+#     else:
+#         form = SurveyForm()
+#     return render(request, 'create_survey.html', {'form': form})
 def create_survey(request):
     if request.method == 'POST':
         form = SurveyForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('survey_list')
+            # Create a survey instance but don't save it to the database yet
+            survey = form.save(commit=False)  # This creates an instance but does not save it
+            
+            # Assign the creator as the currently logged-in user
+            survey.creator = request.user
+            
+            # Now save the survey to the database
+            survey.save()
+            
+            return redirect('survey_list')  # Redirect to the survey list or another page
     else:
         form = SurveyForm()
+
     return render(request, 'create_survey.html', {'form': form})
 
 @login_required(login_url='login') 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True) 
 def edit_survey(request, survey_id):
     survey = get_object_or_404(Survey, id=survey_id)
+    # #neww
+    # if survey.creator != request.user:
+    #     messages.error(request, "You are not authorized to edit this survey.")
+    #     return redirect('survey_list') 
+    # #ends here#
     if request.method == 'POST':
         form = SurveyForm(request.POST, instance=survey)
         if form.is_valid():
@@ -57,6 +96,11 @@ def edit_survey(request, survey_id):
 @cache_control(no_cache=True, must_revalidate=True, no_store=True) 
 def delete_survey(request, survey_id):
     survey = get_object_or_404(Survey, id=survey_id)
+    #     #neww
+    # if survey.creator != request.user:
+    #     messages.error(request, "You are not authorized to delete this survey.")
+    #     return redirect('survey_list') 
+    # #ends here#
     if request.method == 'POST':
         survey.delete()
         return redirect('survey_list')
@@ -66,6 +110,11 @@ def delete_survey(request, survey_id):
 @cache_control(no_cache=True, must_revalidate=True, no_store=True) 
 def create_question(request, survey_id):
     survey = get_object_or_404(Survey, id=survey_id)
+    #     #neww
+    # if survey.creator != request.user:
+    #     messages.error(request, "You are not authorized to edit this survey by creating questions.")
+    #     return redirect('survey_list') 
+    # #ends here#
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
@@ -81,6 +130,11 @@ def create_question(request, survey_id):
 @cache_control(no_cache=True, must_revalidate=True, no_store=True) 
 def edit_question(request, question_id):
     question = get_object_or_404(Question, id=question_id)
+    #     #neww
+    # if survey.creator != request.user:
+    #     messages.error(request, "You are not authorized to edit questions of this survey.")
+    #     return redirect('survey_list') 
+    # #ends here#
     if request.method == 'POST':
         form = QuestionForm(request.POST, instance=question)
         if form.is_valid():
@@ -94,24 +148,36 @@ def edit_question(request, question_id):
 @cache_control(no_cache=True, must_revalidate=True, no_store=True) 
 def delete_question(request, question_id):
     question = get_object_or_404(Question, id=question_id)
+    #     #neww
+    # if survey.creator != request.user:
+    #     messages.error(request, "You are not authorized to edit this survey by deleting questions.")
+    #     return redirect('survey_list') 
+    # #ends here#
     survey_id = question.survey.id
     if request.method == 'POST':
         question.delete()
         return redirect('survey_detail', survey_id=survey_id)
     return render(request, 'confirm_delete.html', {'object': question, 'object_type': 'question'})
 
-def calculate_score(data):
+# def calculate_score(data):
+#     score = 0
+#     for key, value in data.items():
+#         if value == 'A':
+#             score += 1
+#         elif value == 'B':
+#             score += 2
+#         elif value == 'C':
+#             score += 3
+#         elif value == 'D':
+#             score += 4
+#     return score
+
+def calculate_score(responses):
     score = 0
-    for key, value in data.items():
-        if value == 'A':
-            score += 1
-        elif value == 'B':
-            score += 2
-        elif value == 'C':
-            score += 3
-        elif value == 'D':
-            score += 4
+    for question_id, answer in responses.items():
+        score =score + 5 *  int(answer)  # Convert the answer to an integer and add to the score
     return score
+
 
 def loginPage(request): 
     if request.user.is_authenticated: 
